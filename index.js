66aea390a42cd5869ef7e7f81c71f302b8f025a6
George Arshed <erdemerdem11221154@gmail.com>
2026-01-13 12:21:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: 'SuperSecretKeyLevant123',
+    secret: process.env.ClientSecret,
     resave: false,
     saveUninitialized: false,
     cookie: { 
