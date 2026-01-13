dc4c996a2d3dec2ed9759c521b34a11891d6fa41
George Arshed <erdemerdem11221154@gmail.com>
2026-01-13 13:38:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ const MAIN_GUILD_ID = process.env.MAIN_GUILD_ID;
 const BACKEND_URL = process.env.BACKEND_URL || "https://levant-backend.onrender.com";
 const FRONTEND_URL = process.env.FRONTEND_URL || "https://lilzeng1.github.io/Levant";
 
+const ROLE_HIERARCHY = [
+    { id: "1452854057906733257", name: "Founder" },
+    { id: "1452854589668982899", name: "Moderator" },
+    { id: "1452855452504162496", name: "Community Guide" },
+    { id: "1452856001605927023", name: "Helper" },
+    { id: "1452857818909769768", name: "Event Lead" },
+    { id: "1453136624220246183", name: "Levant Booster" },
+    { id: "1453527525350178957", name: "Ascendant" },
+    { id: "1452856702302158868", name: "Content Creator" },
+    { id: "1452856780140052681", name: "Musician" },
+    { id: "1456104633234886666", name: "Core Supporter" }
+];
+
 // DISCORD CLIENT
 const client = new Client({
     intents: [
@@ -39,7 +52,7 @@ const User = mongoose.model("User", userSchema);
 const app = express();
 
 app.use(cors({
-    origin: FRONTEND_URL, 
+    origin: ["https://lilzeng1.github.io", "http://127.0.0.1:5500"], 
     methods: ['GET', 'POST'],
     credentials: true
 }));
@@ -115,6 +128,23 @@ app.get('/api/auth/discord/redirect', async (req, res) => {
     } catch (error) {
         console.error('Auth Error:', error.response ? error.response.data : error.message);
         res.status(500).send('Authentication Failed');
+    }
+});
+
+// UpdateNickName()
+app.post('/api/user/update-nick', async (req, res) => {
+    const { userId, nickname } = req.body;
+    try {
+        const guild = client.guilds.cache.get(MAIN_GUILD_ID);
+        const member = await guild.members.fetch(userId);
+        if (member) {
+            await member.setNickname(nickname);
+            return res.json({ success: true });
+        }
+        res.status(404).send("Member not found");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Nickname update failed (Bot permissions?)");
     }
 });
 
